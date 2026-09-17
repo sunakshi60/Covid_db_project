5d143c21190987f3c6d9bd0ce7fdd39b73afdade
Sunakshi <sunakshiisharma0060@gmail.com>
2026-09-17 11:26:41 +0530

    UC2: Country death

--- sql/solutions.sql
+++ sql/solutions.sql
@@ -9,3 +9,15 @@ JOIN country c
 WHERE g.report_date = DATE '2021-09-30'
 ORDER BY g.confirmed DESC
 LIMIT 1;
+
+--2.	Show the total number of deaths in each country, including provinces/states, for a given date.
+SELECT
+    c.name AS country,
+    g.report_date,
+    SUM(g.deaths) AS total_deaths
+FROM global_covid_stats g
+JOIN country c
+    ON g.country_id = c.country_id
+WHERE g.report_date = DATE '2021-09-30'
+GROUP BY c.name, g.report_date
+ORDER BY total_deaths DESC;

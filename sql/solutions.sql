44547715413faea7250c3843f1264b380d97fcf2
Sunakshi <sunakshiisharma0060@gmail.com>
2026-09-17 11:15:58 +0530

    UC1: Country highest cases

--- sql/solutions.sql
+++ sql/solutions.sql
@@ -1,1 +1,11 @@
-﻿
+﻿-- 1.	Which country has the highest number of confirmed cases on a specific date?
+SELECT
+    c.name AS country,
+    g.report_date,
+    g.confirmed
+FROM global_covid_stats g
+JOIN country c
+    ON g.country_id = c.country_id
+WHERE g.report_date = DATE '2021-09-30'
+ORDER BY g.confirmed DESC
+LIMIT 1;
